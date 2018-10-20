88295574c64a9fdfe8b3a5c39ed7f14388ca02be
lostFatRivers <495669631@qq.com>
2018-10-20 15:26:52 +0800

    add show

--- JavaScript/Vue/btsd/js/bt.js
+++ JavaScript/Vue/btsd/js/bt.js
@@ -1,6 +1,24 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        curPage: "indexPage"
+        curPage: "page_index"
+    },
+    created() {
+        this.curPage = window.location.hash.slice(1) || "page_index";
+        window.addEventListener('hashchange', () => {
+            this.curPage = window.location.hash.slice(1) || "page_index";
+        });
+    },
+    methods: {
+
+    },
+    watch: {
+        curPage: {
+            handler() {
+                if (this.curPage == 'page_index') {
+                    
+                }
+            }
+        }
     }
 });
