847f20059b564911ba10a6679f0d27cc21cc1f0f
lostFatRivers <495669631@qq.com>
2018-11-14 09:37:23 +0800

    add ws data

--- JavaScript/teach/test.js
+++ JavaScript/teach/test.js
@@ -1,3 +1,18 @@
+const Constants = {
+    // 登录
+    CS_LOGIN: 1001,
+    // 登录反馈
+    SC_LOGIN: 1002,
+    // 设置范围
+    CS_SET_AREA: 1003,
+    // 设置范围反馈
+    SC_SET_AREA: 1004,
+    // 发送聊天消息
+    CS_CHAT_MSG: 1005,
+    // 接收聊天消息
+    SC_CHAT_MSG: 1006
+}
+
 class Robot {
     constructor(id, puid, name, age, sex, city, profile) {
         this.id = id;
@@ -35,15 +50,41 @@ class ChatContent {
 let robotBar = {
     props: ['activeId', 'robot'],
     template: '#temp-robot-bar',
+    computed: {
+        hasNewMessage() {
+            for (let eachChat of this.robot.chats) {
+                let notReadNum = eachChat.chatContent.filter(ec => ec.id == eachChat.friendId).length - eachChat.readNum;
+                if (notReadNum > 0) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        robotIcon() {
+            return {
+                backgroundImage: "url('./robotIcon" + this.robot.sex + ".png')"
+            };
+        }
+    }
 };
 
 let robotChatBar = {
     props: ['activeId', 'chat'],
-    template: '#temp-robot-chat-bar'
+    template: '#temp-robot-chat-bar',
+    computed: {
+        notReadNum() {
+            return this.chat.chatContent.filter(ec => ec.id == this.chat.friendId).length - this.chat.readNum;
+        },
+        friendIcon() {
+            return {
+                backgroundImage: "url('./friendIcon" + this.chat.sex + ".png')"
+            };
+        }
+    }
 };
 
 let chatBubble = {
-    props: ['bubble', 'friend'],
+    props: ['bubble', 'friend', 'robotsex', 'friendsex'],
     template: '#temp-chat-bubble',
     data() {
         return {
@@ -51,6 +92,21 @@ let chatBubble = {
                 float: this.bubble.id == this.friend ? 'left' : 'right',
                 marginLeft: this.bubble.id == this.friend ? '20px' : '0px',
                 marginRight: this.bubble.id == this.friend ? '0px' : '20px',
+            },
+            iconBgSwitch: {
+                backgroundImage: "url('./" + (this.bubble.id == this.friend ? 'friendIcon' : 'robotIcon') + (this.bubble.id == this.friend ? this.friendsex : this.robotsex) + ".png')"
+            }
+        }
+    },
+    watch: {
+        robotsex() {
+            this.iconBgSwitch = {
+                backgroundImage: "url('./" + (this.bubble.id == this.friend ? 'friendIcon' : 'robotIcon') + (this.bubble.id == this.friend ? this.friendsex : this.robotsex) + ".png')"
+            }
+        },
+        friendsex() {
+            this.iconBgSwitch = {
+                backgroundImage: "url('./" + (this.bubble.id == this.friend ? 'friendIcon' : 'robotIcon') + (this.bubble.id == this.friend ? this.friendsex : this.robotsex) + ".png')"
             }
         }
     }
@@ -63,9 +119,14 @@ let vm = new Vue({
         maxId: -1,
         robotArray: [
         ],
-        curRobot: null,
-        isLogin: true,
-        editContent: ''
+        curRobot: undefined,
+        isLogin: false,
+        editContent: "",
+        loginSecret: '',
+        minRobotId: 0,
+        maxRobotId: 0,
+        websocketConn: null,
+        connectStatus: ""
     },
     created() {
         this.initData();
@@ -73,16 +134,45 @@ let vm = new Vue({
     components: {
         'robot-bar': robotBar,
         'robot-chat-bar': robotChatBar,
-        'chat-bubble' : chatBubble
+        'chat-bubble': chatBubble
     },
     methods: {
-        shootMessage() {
-            if (!this.curRobot || !this.curRobot.curChat || this.editContent.trim() === '') {
-                this.editContent = '';
+        loginRobotConsole() {
+            let secretStr = this.loginSecret;
+            this.sendMessage(JSON.stringify({type: Constants.CS_LOGIN, secret: secretStr}));
+        },
+        setupRobotArea() {
+            let value = prompt("选取机器人范围, 中间用 ',' 隔开 :").trim();
+            if (value == '' || value.split(',').length != 2) {
+                alert('输入有误, 请重新输入.');
                 return;
             }
-            this.curRobot.curChat.chatContent.push(new ChatContent(this.curRobot.id, this.editContent));
-            this.editContent = '';
+            let valSplit = value.split(',');
+            this.minRobotId = parseInt(valSplit[0]);
+            this.maxRobotId = parseInt(valSplit[1]);
+            this.sendMessage(JSON.stringify({
+                type: Constants.CS_SET_AREA,
+                secret: this.loginSecret,
+                minId: this.minRobotId,
+                maxId: this.maxRobotId
+            }))
+        },
+        keyupHandler(ev) {
+            if (ev.keyCode == 13) {
+                if (ev.ctrlKey) {
+                    this.editContent += "\n";
+                } else {
+                    this.shootMessage();
+                }
+            } 
+        },
+        shootMessage() {
+            if (!this.curRobot || !this.curRobot.curChat || this.editContent.trim() === '') {
+                this.editContent = "";
+                return;
+            }
+            this.curRobot.curChat.chatContent.push(new ChatContent(this.curRobot.id, this.editContent.trim()));
+            this.editContent = "";
             Vue.nextTick(() => {
                 let editerDom = document.getElementById('chat-scroll-content');
                 editerDom.scrollTop = editerDom.scrollHeight;
@@ -100,30 +190,116 @@ let vm = new Vue({
             });
         },
         initData() {
-            axios.get('./robot.json').then(res => {
-                // success
-                for (let er of res.data.robot) {
-                    let robot = new Robot(er.id, er.puid, er.name, er.age, er.sex, er.city, er.profile);
-                    for (let ec of er.chats) {
-                        let chat = new RobotChat(ec.friendId, ec.friendName, ec.age, ec.sex);
-                        for (let ecc of ec.chatContent) {
-                            let chatC = new ChatContent(ecc.id, ecc.chat)
-                            chat.chatContent.push(chatC);
-                        }
-                        robot.chats.push(chat);
-                        if (!robot.curChat) {
-                            robot.curChat = chat;
-                        }
+            if (!WebSocket) {
+                alert("浏览器版本太低, 请换个浏览器重新打开.");
+                return;
+            }
+            this.websocketConn = new WebSocket('ws://192.168.1.192:10800/websocket');
+            this.websocketConn.onopen = ev => {
+                this.connectStatus = "已连接...";
+            };
+            this.websocketConn.onmessage = ev => {
+                this.messageHandler(ev.data);
+            };
+            this.websocketConn.onclose = ev => {
+                this.isLogin = false;
+                this.connectStatus = "已断开...";
+            }
+        },
+        sendMessage(jsonStr) {
+            if (this.websocketConn.readyState != 1) {
+                return;
+            }
+            this.websocketConn.send(jsonStr);
+            console.log('send msg', jsonStr);
+        },
+        messageHandler(receiveData) {
+            let msg = JSON.parse(receiveData);
+            console.log('receive msg', receiveData);
+            if (msg.type == Constants.SC_LOGIN) {
+                let status = msg.status;
+                if (status == "SUCCESS") {
+                    this.connectStatus = "已登录...";
+                    this.isLogin = true;
+                } else {
+                    this.connectStatus = "登录失败...";
+                    this.loginSecret="";
+                }
+            } else if (msg.type == Constants.SC_SET_AREA) {
+                let status = msg.status;
+                if (status == "SUCCESS") {
+                    this.assembleRobotData(msg.robotData.robot);
+                } else {
+                    alert("范围选取失败");
+                    this.minRobotId = 0;
+                    this.maxRobotId = 0;
+                }
+            } else if (msg.type == Constants.SC_CHAT_MSG) {
+                
+            }
+        },
+        assembleRobotData(robotData) {
+            for (let er of robotData) {
+                let robot = new Robot(er.id, er.puid, er.name, er.age, er.sex, er.city, er.profile);
+                if (strLen(er.name) > 6) {
+                    robot.simpleName = subStr(er.name, 6) + "...";
+                }
+                for (let ec of er.chats) {
+                    let chat = new RobotChat(ec.friendId, ec.friendName, ec.age, ec.sex);
+                    for (let ecc of ec.chatContent) {
+                        let chatC = new ChatContent(ecc.id, ecc.chat)
+                        chat.chatContent.push(chatC);
                     }
-                    this.robotArray.push(robot);
-                    if (!this.curRobot) {
-                        this.curRobot = robot;
+                    robot.chats.push(chat);
+                    if (!robot.curChat) {
+                        robot.curChat = chat;
                     }
                 }
-            }, err => {
-                // error
-                console.log(err);
-            });
+                this.robotArray.push(robot);
+                if (!this.curRobot) {
+                    this.curRobot = robot;
+                }
+            }
+        }
+    },
+    computed: {
+        robotDetailIcon() {
+            return {
+                backgroundImage: "url('./robotIcon" + this.curRobot.sex + ".png')"
+            };
         }
     }
 });
+
+function strLen(str) {
+    let len = 0;
+    for (let i = 0; i < str.length; i++) {
+        let c = str.charCodeAt(i);
+        if ((c >= 0x0001 && c <= 0x007e) || (0xff60 <= c && c <= 0xff9f)) {
+            len++;
+        }
+        else {
+            len += 2;
+        }
+    }
+    return len;
+}
+
+function subStr(str, limitLen) {
+    let len = 0;
+    let res = '';
+    for (let i = 0; i < str.length; i++) {
+        if (len >= limitLen) {
+            return res;
+        }
+        let c = str.charCodeAt(i);
+        res += str[i];
+        if ((c >= 0x0001 && c <= 0x007e) || (0xff60 <= c && c <= 0xff9f)) {
+            len++;
+        }
+        else {
+            len += 2;
+        }
+    }
+    return res;
+}
