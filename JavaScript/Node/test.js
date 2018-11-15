d5a008d593009dfade47f13dffb1c9e6bcd31240
lostFatRivers <495669631@qq.com>
2018-11-15 17:15:52 +0800

    restudy vue

--- JavaScript/Node/test.js
+++ JavaScript/Node/test.js
@@ -1,2 +1,16 @@
-console.log('A'.charCodeAt(0));
-console.log('Z'.charCodeAt(0));
+class MockVue {
+    constructor(obj) {
+        this.obj = obj;
+        this.bbq = "你是谁";
+    }
+}
+
+let mv = new MockVue({
+    computed: {
+        remessage() {
+            return this.bbq;
+        }
+    }
+});
+
+console.log(mv.obj.computed.remessage());
